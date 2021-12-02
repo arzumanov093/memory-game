8483fccf9fcbfcd0a2150972bb3e16d4fde6f030
Arkadiy <90386000+arzumanov093@users.noreply.github.com>
2021-12-02 10:14:30 +0300

    update images URL

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,14 @@ import { Card } from '../Card/Card';
 import './App.css';
 
 const cardImages = [
-  {"src": "/img/arsenal.png", matched: false},
-  {"src": "/img/chelsea.png", matched: false},
-  {"src": "/img/everton.png", matched: false},
-  {"src": "/img/leicester.png", matched: false},
-  {"src": "/img/liverpool.png", matched: false},
-  {"src": "/img/mc.png", matched: false},
-  {"src": "/img/mu.png", matched: false},
-  {"src": "/img/spurs.png", matched: false}
+  {"src": "/img/arsenal.png" || "https://leadersinsport.com/wp-content/uploads/2020/09/arsenal-logo-symbol-arsenal-stl-model-grb-stl-arsenal-21.png", matched: false},
+  {"src": "/img/chelsea.png" || "http://2.bp.blogspot.com/-YrdKwVC7hKY/UIquaZydGsI/AAAAAAAAAWk/Ay5KCQSsUiM/s1600/Logo+Chelsea+FC.png", matched: false},
+  {"src": "/img/everton.png" || "https://pixy.org/src/87/878799.png", matched: false},
+  {"src": "/img/leicester.png" || "https://toplogos.ru/images/logo-leicester-city-fc.png", matched: false},
+  {"src": "/img/liverpool.png" || "https://w7.pngwing.com/pngs/151/663/png-transparent-liver-pool-football-club-logo-anfield-liverpool-f-c-everton-f-c-dream-league-soccer-2010-11-premier-league-liverpool-fc-thumbnail.png", matched: false},
+  {"src": "/img/mc.png" || "https://w1.pngwing.com/pngs/203/356/png-transparent-premier-league-logo-manchester-manchester-city-fc-organization-line-area-circle-symbol.png", matched: false},
+  {"src": "/img/mu.png" || "https://clipart-best.com/img/manchester_united/manchester_united-clip-art-5.png", matched: false},
+  {"src": "/img/spurs.png" || "https://w7.pngwing.com/pngs/863/950/png-transparent-tottenham-hotspur-f-c-fa-cup-ossie-s-dream-premier-league-football-premier-league.png", matched: false}
 ];
 
 function App() {
