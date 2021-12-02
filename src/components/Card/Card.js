6e32cf733a2c9594cd3df4bb875a14210aace45b
Arkadiy <90386000+arzumanov093@users.noreply.github.com>
2021-12-02 10:20:06 +0300

    update images URL

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -19,7 +19,8 @@ export const Card = ({card, handleChoice, flipped, disabled}) => {
                 />
                 <img 
                     className='back' 
-                    src='/img/cover.png'
+                    // src='/img/cover.png'
+                    src='https://www.infosasa.com/wp-content/uploads/2015/12/premier_league_new_logo.jpg'
                     alt='card back'
                     onClick={handleClick}
                 />
